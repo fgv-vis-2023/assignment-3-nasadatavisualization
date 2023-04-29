507350a42e9aa0881164b95a9525bffbf2688c12
barrafas <barradastiago15@gmail.com>
2023-04-28 23:18:49 -0300

    adicionando legenda e valores numericos no grafico de barras

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+// only relevant quantitative data
+var bar_vars = ['Absolute Magnitude', 'Est Dia in KM(min)', 'Est Dia in KM(max)', 'Relative Velocity km per sec', 'Miss Dist.(kilometers)', 'Minimum Orbit Intersection', 'Jupiter Tisserand Invariant', 'Eccentricity', 'Semi Major Axis', 'Inclination', 'Asc Node Longitude', 'Orbital Period', 'Perihelion Distance', 'Perihelion Arg', 'Aphelion Dist', 'Mean Anomaly', 'Mean Motion']
+
 var column = "Absolute Magnitude"
 
     // define a function to load the data
     function loadData() {
     d3.csv("nasa.csv", function(data) {
-
         // Create Meteor Dropdown
         d3.select("#meteors")
             .selectAll("option")
@@ -17,7 +19,7 @@ var column = "Absolute Magnitude"
         // Create Column Dropdown
         d3.select("#columns")
             .selectAll("option")
-            .data(data.columns)
+            .data(bar_vars)
             .enter()
             .append("option")
             .text(function(d) {
@@ -32,27 +34,53 @@ var column = "Absolute Magnitude"
         // Create Column Scale
         var columnScale = d3.scaleLinear()
             .domain([0, d3.max(data, d => d[column])])
-            .range([0, 316]);
+            .range([0, 316-30]); // Offset to account for text
         
+
+        const sidebar = d3.select("#sideBarPlotGroup")
+
         // Erase previous Side Bar plot
-        d3.select("#sideBarPlotGroup").selectAll('rect').remove()
+        sidebar.selectAll('rect').remove()
+        sidebar.selectAll('text').remove()
 
         // Create Side Bar Plot
-        d3.select("#sideBarPlotGroup").selectAll('rect')
+        sidebar.selectAll('rect')
             .data(data)
             .enter()
             .append('rect')
-            .attr('x', 15 )
-            .attr('y', (d, i) => 50 + i * 20 )
-            .attr('width', d => columnScale(d[column]))
-            .attr('height', 18)
-            .attr('fill', d => d["Hazardous"] == "True" ? '#c1121f' : '#003459')
-        .append('title')
-            .text(d => d["Name"]);
+                .attr('x', 15 )
+                .attr('y', (d, i) => 50 + i * 20 )
+                .attr('width', d => columnScale(d[column]))
+                .attr('height', 18)
+                .attr('fill', d => d["Hazardous"] == "True" ? '#c1121f' : '#003459')
+            .append('title')
+                .text(d => "ID: " + d["Name"] + " Valor: " + d[column]);
         
         // Crete Side Bar Plot Title
         d3.select("#sideBarTitle")
             .text(column);
+
+        sidebar.append('g')
+            .attr("class", "value-label")
+            .selectAll('text')
+            .data(data)
+            .enter()
+            .append('text')
+                .attr('x', d => columnScale(d[column])+18)
+                .attr('y', (d, i) => i*20+64)
+                .text(d => Math.round(d[column]*100)/100)
+                .attr('fill', 'Gray');
+
+        sidebar.append('g')
+            .attr("class", "id-label")
+            .selectAll('text')
+            .data(data)
+            .enter()
+            .append('text')
+                .attr('x', 18)
+                .attr('y', (d, i) => i*20+64)
+                .text(d => "ID: " + d["Name"])
+                .attr('fill', 'white');
     })
     }
 
